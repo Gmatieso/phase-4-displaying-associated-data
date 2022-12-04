dc6f6ddf13a8148b45e1bafd34f716811d0a90ee
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-04 12:05:53 +0300

    Documenting our review

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -4,6 +4,7 @@ class DogHousesController < ApplicationController
   def show
     dog_house = DogHouse.find(params[:id])
     render json: dog_house, include: :reviews
+    #using include reviews will call the .reviews method that is provided has_many and will serialize the reviews as a nested array of JSON data.
   end
 
   private
